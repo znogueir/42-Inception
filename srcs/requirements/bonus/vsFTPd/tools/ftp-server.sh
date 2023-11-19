7867608df6363248b0cccedc271df8b754dcec70
znogueir <zachary.nogueira@gmail.com>
2023-11-19 21:33:43 +0100

    FTP works All thanks to OG proplayer miguel otero

--- srcs/requirements/bonus/vsFTPd/tools/ftp-server.sh
+++ srcs/requirements/bonus/vsFTPd/tools/ftp-server.sh
@@ -3,10 +3,11 @@
 #if [ ! -f "/var/run/vsftpd/vsftpd.conf.bak" ]; then
 
     mkdir -p /var/www/html
-    mkdir -p /var/run/vsftpd/
+    mkdir -p /etc/vsftpd/
+    #mkdir -p /var/run/vsftpd/
     #chmod 755 -R
     #cp /var/run/vsftpd/vsftpd.conf /var/run/vsftpd/vsftpd.conf.bak
-    mv /etc/vsftpd.conf /var/run/vsftpd/vsftpd.conf
+    mv /tmp/vsftpd.conf /etc/vsftpd.conf
 
     # Add the FTP_USER, change his password and declare him as the owner of wordpress folder and all subfolders
     adduser $FTP_USER --disabled-password
@@ -14,9 +15,9 @@
     chown -R $FTP_USER:$FTP_USER /var/www/html
 
     #chmod +x /etc/vsftpd/vsftpd.conf
-    echo $FTP_USER | tee -a /var/run/vsftpd.userlist
+    echo $FTP_USER | tee -a /etc/vsftpd.userlist
 
 #fi
 
 echo "vsFTPd started on port 2121"
-/usr/sbin/vsftpd /var/run/vsftpd/vsftpd.conf
+/usr/sbin/vsftpd /etc/vsftpd.conf
